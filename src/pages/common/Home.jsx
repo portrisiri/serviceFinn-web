436be886d693ce94af42f6f7230d43cbde18d4ae
sittakarn-m <sittakarn.m@gmail.com>
2025-03-13 15:44:27 +0700

    ready to push

--- src/pages/common/Home.jsx
+++ src/pages/common/Home.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import HomeSections from '../../components/common/HomeSections'
-import Searchbar from '../../components/common/Searchbar'
-
+import React from "react";
+import HomeSections from "../../components/common/HomeSections";
+import Searchbar from "../../components/common/Searchbar";
+import SidebarProvider from "../../components/provider/SidebarProvider";
 
 function Home() {
   return (
     <div>
-
-      <HomeSections/>
+      <HomeSections />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
